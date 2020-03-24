2ce10504e0283b944eb9301d0416989613a2b31c
Rod Conan <rconan@gmto.org>
2020-03-24 23:38:00 +0000

    AGWS TEST PROBES UPDATE

--- src/bin/agws_probes.rs
+++ src/bin/agws_probes.rs
@@ -22,22 +22,62 @@ fn main() {
      */
 
     let mut obs = astrotools::Observation::new(
-        "2020-03-20T00:00:00",
+        "2012-06-10T04:01:03",
         astrotools::GMT_LAT,
         astrotools::GMT_LONG,
     );
-    let t = astrotools::SkyCoordinates::new(0., 0.);
-    let p = astrotools::SkyCoordinates::new(0., 0.);
+    let t = astrotools::SkyCoordinates::new(266.62156258190726, -27.776114821065107);
+    let p = astrotools::SkyCoordinates::new(266.62156258190726, -27.776114821065107);
 
     let mut hprobe = agws::probe::Handler::new(
-        obs,
-        t,
         p,
         0.,
         agws::probe::SH48::new(),
         (probe_channel.0, telescope_channel.1),
     );
-    hprobe.init();
+    hprobe.init(&obs, &t);
     hprobe.sensor.through();
-    println!("{:?}", hprobe.sensor.guide_star.wfe_rms_10e(-9));
+    println!("{:?}", hprobe.sensor.get_guide_star().wfe_rms_10e(-9));
+    obs.add_seconds(30.);
+    println!("{}", obs.datetime);
+    hprobe.update(&obs, &t, None, None);
+
+    
+    let m1_rbm: Vec<Vec<f64>> = vec![vec![0.; 6]; 7];
+    let m2_rbm: Vec<Vec<f64>> = vec![vec![0.; 6]; 7];
+    obs.add_seconds(30.);
+    println!("{}", obs.datetime);
+    hprobe.update(&obs, &t, Some(m1_rbm), Some(m2_rbm));
+
+    let stt0 = hprobe.sensor.get_guide_star().segments_gradients();
+
+    let mut obs = astrotools::Observation::new(
+        "2012-06-10T04:01:03",
+        astrotools::GMT_LAT,
+        astrotools::GMT_LONG,
+    );
+    let t = astrotools::SkyCoordinates::new(266.62156258190726, -27.776114821065107);
+    let p = astrotools::SkyCoordinates::new(266.62156258190726, -27.776114821065107);
+
+    for k in 0..10 {
+        println!("{}", obs.datetime);
+
+        let mut m1_rbm: Vec<Vec<f64>> = vec![vec![0.; 6]; 7];
+        let mut m2_rbm: Vec<Vec<f64>> = vec![vec![0.; 6]; 7];
+        m1_rbm[k%7][3] = 1e-6;
+        m2_rbm[k%7][4] = 1e-6;
+        hprobe.update(&obs, &t, Some(m1_rbm), Some(m2_rbm));
+        let mut stt = hprobe.sensor.get_guide_star().segments_gradients();
+
+        for a in 0..2 {
+            for i in 0..7 {
+                stt[a][i] -= stt0[a][i];
+                stt[a][i] *= 1e6;
+            }
+        }
+
+
+        println!("{:.2?}", stt);
+        obs.add_seconds(30.);
+    }
 }
