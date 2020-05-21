ebb396f57cbaded09916469832f8f6af59d35681
Rod Conan <rconan@gmto.org>
2020-05-21 15:06:13 +0000

    MULTI-THREAD M1_PRINTTHROUGH

--- src/bin/m1_printthrough.rs
+++ src/bin/m1_printthrough.rs
@@ -1,6 +1,8 @@
-use csv::Reader;
+use csv::{Reader, Writer};
 use gicsdom::astrotools;
-use gicsdom::ceo::{Conversion, Gmt, PSSn, Source};
+use gicsdom::ceo::{set_gpu, Conversion, Gmt, PSSn, Source};
+use rayon;
+use rayon::prelude::*;
 use serde::Deserialize;
 use std::{f64, time::Instant};
 
@@ -70,6 +72,27 @@ fn anti_gravity(alt: f64) -> Vec<f64> {
     ag.append(&mut u);
     ag
 }
+#[test]
+fn m1_local_gravity() {
+    let mut ag = anti_gravity(90_f64.to_radians());
+    let mut e = 0_f64;
+    for k in 0..7 {
+        let ag_k = &ag[3 * k..3 * (k + 1)];
+        println!(
+            "[{}]",
+            ag_k.iter()
+                .map(|x| format!("{:.9}", x))
+                .collect::<Vec<String>>()
+                .as_slice()
+                .join(",")
+        );
+        if k < 6 {
+            e += (ag_k[1] - 0.23381871).powi(2) + (ag_k[2] - -0.02771979).powi(2);
+        }
+    }
+    println!("e: {}", e.sqrt());
+    assert!(e < 1e-6);
+}
 
 fn pssn_temporal_stability(
     field: &StarField,
@@ -80,7 +103,7 @@ fn pssn_temporal_stability(
     for result in rdr.deserialize() {
         science_field.push(result.unwrap());
     }
-    println!("{:?}", science_field);
+    //println!("{:?}", science_field);
     let mut src = Source::new(science_field.len() as i32, 25.5, 401);
     let zen = science_field
         .iter()
@@ -96,12 +119,10 @@ fn pssn_temporal_stability(
     let mut gmt = Gmt::new();
     gmt.build_m1("M1_printthrough", 3).build_m2(None);
     src.through(&mut gmt).xpupil();
-    println!("WFE RMS: {:.3}nm", src.wfe_rms_10e(-9)[0]);
+    //println!("WFE RMS: {:.3}nm", src.wfe_rms_10e(-9)[0]);
 
     let mut pssn = PSSn::new(0.15, 25.0);
     pssn.build(&mut src);
-    println!("PSSn: {:?}", pssn.reset(&mut src).estimates);
-    println!("PSSn s.u.: {}", pssn.spatial_uniformity());
 
     //println!("{}", field.datetime);
     let star_field_time = astrotools::Time::from_iso_8601(&field.datetime);
@@ -123,19 +144,21 @@ fn pssn_temporal_stability(
         duration,
     );
     let (alt, az) = obs.altaz();
+    /*
     println!(
         " == {} - [{:.3},{:.3}]",
         obs.utc.datetime(),
         alt.to_degrees(),
         az.to_degrees()
     );
+    */
+    pssn.reset(&mut src);
     let psu = pssn.spatial_uniformity();
-    println!("PSSn: {} - PSU: {:.3}%", pssn, psu);
+    //println!("PSSn: {} - PSU: {:.3}%", pssn, psu);
 
     let mut sorted_field_pssn: Vec<Vec<f32>> = vec![Vec::with_capacity(8); 21];
     let mut psu: Vec<f32> = Vec::with_capacity(8);
 
-    let now = Instant::now();
     while let Some(_) = obs.next() {
         let (alt, az) = obs.altaz();
         let alt_deg = alt.to_degrees();
@@ -147,21 +170,27 @@ fn pssn_temporal_stability(
         src.through(&mut gmt).xpupil();
         pssn.accumulate(&mut src);
         if (obs.step % peek_lag) == 0 {
+            /*
             println!(
                 " == {} - [{:.3},{:.3}]",
                 obs.utc.datetime(),
                 alt_deg,
                 az.to_degrees()
             );
+            */
             pssn.peek(&mut src);
             psu.push(pssn.spatial_uniformity());
-            println!("PSSn: {} - PSU: {:.3}%", pssn, psu.last().unwrap());
+            //println!("PSSn: {} - PSU: {:.3}%", pssn, psu.last().unwrap());
 
             for k in 0..21 {
                 sorted_field_pssn[k].push(pssn.estimates[k])
             }
         }
     }
+
+    drop(src);
+    drop(gmt);
+    drop(pssn);
 
     let pssn_on_axis = sorted_field_pssn[0][0];
     let mut median_field_pssn = vec![];
@@ -175,29 +204,13 @@ fn pssn_temporal_stability(
         / mean_median_field_pssn;
     //println!("{:?}", median_field_pssn);
     //println!("{:?}", mean_median_field_pssn);
-    println!("PTS: {:.3}%", pts);
+    //println!("PTS: {:.3}%", pts);
 
-    println!("ET: {}s", now.elapsed().as_secs());
     Ok((pssn_on_axis, psu[0], pts))
 }
 fn main() {
-    /*
-    let mut ag = anti_gravity(30_f64.to_radians());
-    for k in 0..7 {
-        println!(
-            "[{}]",
-            ag[3 * k..3 * (k + 1)]
-                .iter()
-                .map(|x| format!("{:.9}", x))
-                .collect::<Vec<String>>()
-                .as_slice()
-                .join(",")
-        );
-    }
-    */
-
     let mut star_field: Vec<StarField> = vec![];
-    let mut rdr = Reader::from_path("guide_stars05.csv").unwrap();
+    let mut rdr = Reader::from_path("guide_stars05.csv").expect("File with star fields missing!");
     for result in rdr.deserialize() {
         match result {
             Ok(record) => star_field.push(record),
@@ -206,12 +219,38 @@ fn main() {
     }
     println!("# star field: {}", star_field.len());
 
-    let star_field_id = 1;
-    match pssn_temporal_stability(&star_field[star_field_id], 60_f64) {
-        Ok((pssn, psu, pts)) => println!(
-            "KPP PSSn:\n * O.A.  : {:.4}\n * S.U.  : {:.3}%\n * P.T.S : {:.3}%",
-            pssn, psu, pts
-        ),
-        Err(e) => println!("{}", e),
+    let now = Instant::now();
+
+    let sampling_time = 10_f64;
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(32)
+        .build()
+        .unwrap();
+    //let star_field_id = 0;
+  
+    //star_field.truncate(8);
+    let results = pool.install(|| {
+        star_field
+            .par_iter()
+            .map(|field| {
+                let thread_id = pool.current_thread_index().unwrap();
+                //        println!("Thread ID#{}",thread_id);
+                set_gpu((thread_id % 4) as i32);
+                pssn_temporal_stability(&field, sampling_time)
+            })
+            .collect::<Vec<Result<(f32, f32, f32), String>>>()
+    });
+//    println!("{:?}", results);
+
+    println!("ET: {}s", now.elapsed().as_secs());
+
+    let mut wtr = Writer::from_path("KPP_M1_Printthrough.csv").unwrap();
+    wtr.write_record(&["index","oa","psu","pts"]); 
+    for (r,f) in results.iter().zip(star_field) {
+        match r {
+            Ok((pssn, psu, pts)) => wtr.serialize((f.index,pssn,psu,pts)).unwrap(),
+            Err(e) => println!("Star field #{} out of elevation range!",f.index),
+        }
     }
+    wtr.flush().unwrap();
 }
