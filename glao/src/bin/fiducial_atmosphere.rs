3c89cbe33a7a11fe641db2882dbde0ffe5d8e27f
Rod Conan <rconan@gmto.org>
2020-10-13 22:14:49 +0000

    FIDUCIAL ATMOSPHERE
    fiducial_atmosphere with Delaunay21 and same outputs as GLAO

--- glao/src/bin/fiducial_atmosphere.rs
+++ glao/src/bin/fiducial_atmosphere.rs
@@ -1,15 +1,27 @@
 use ceo::{ceo, element, Conversion, CEO};
 use cirrus;
-use serde_pickle as pickle;
-use std::fs::File;
+use glao::glao_sys::ScienceField;
+use serde::{Deserialize, Serialize};
 use std::time::Instant;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Results {
+    time: Vec<f64>,
+    wfe: Vec<(Vec<f32>, Vec<f32>, Vec<f32>)>,
+    atm_fwhm_x: f64,
+    fwhm: Vec<f64>,
+    pssn: Vec<f32>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let n_px = 769;
 
     let mut gmt = ceo!(element::GMT);
-    let mut src = ceo!(element::SOURCE, set_pupil_sampling = [n_px]);
+    //    let mut src = ceo!(element::SOURCE, set_pupil_sampling = [n_px]);
+    let mut science = ScienceField::delaunay_21("Vs", n_px, None);
+    //let mut science = ScienceField::on_axis("Vs", n_px, None);
+    science.build();
 
     let secz = 1f32 / 30f32.to_radians().cos();
     let turb_cn2_height = [275, 425, 1250, 4000, 8000, 13000]
@@ -23,8 +35,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Loading the atmosphere ...");
     atm.raytrace_build(
-        0.14,
-        25.0,
+        science.pssn.r0(),
+        science.pssn.oscale,
         6,
         turb_cn2_height,
         turb_cn2_xi0.to_vec(),
@@ -43,23 +55,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let duration = 400f64;
     let n_sample = (duration * rate) as usize;
     let now = Instant::now();
-    let wfe_rms = (0..n_sample)
+    let mut t: Vec<f64> = vec![];
+    let wfe = (0..n_sample)
         .map(|k| {
             atm.secs = k as f64 / rate;
-            src.through(&mut gmt)
+            t.push(atm.secs);
+            let wfe_rms = science
+                .src
+                .through(&mut gmt)
                 .xpupil()
                 .through(&mut atm)
-                .wfe_rms_10e(-9)[0]
+                .through(&mut science.pssn)
+                .wfe_rms_10e(-9);
+            (
+                wfe_rms,
+                science.src.segment_wfe_rms_10e(-9),
+                science.src.segment_piston_10e(-9),
+            )
         })
-        .collect::<Vec<f32>>();
+        .collect::<Vec<(Vec<f32>, Vec<f32>, Vec<f32>)>>();
     let et = now.elapsed().as_secs();
     println!("Elapsed time: {}s", et);
 
-    let key = format!("GLAO/fiducial_atmosphere_wfe_rms_et{}.pkl", et);
+    let key = "GLAO/fiducial_atmosphere_wfe.pkl";
     //    let mut file = File::create(format!("fiducial_atmosphere_wfe_rms_et{}.pkl",et)).unwrap();
     //    pickle::to_writer(&mut file, &wfe_rms, true);
 
-    cirrus::dump("us-west-2", "gmto.modeling", &key, &wfe_rms).await?;
+    //    cirrus::dump("us-west-2", "gmto.modeling", &key, &wfe_rms).await?;
+    let (atm_fwhm_x, fwhm) = science.glao_wrap_up();
+    let results = Results {
+        time: t,
+        wfe,
+        atm_fwhm_x,
+        fwhm,
+        pssn: science.pssn.peek().estimates.clone(),
+    };
+    println!("Uploading results to: s3://gmto.modeling/{}", key);
+    cirrus::dump("us-west-2", "gmto.modeling", &key, &results).await?;
 
     Ok(())
 }
