8c692aad8cdb86754f6fcd388265ff91248f87ca
Rod Conan <rconan@gmto.org>
2020-10-29 20:21:36 +0000

    TOMOGRAPHY POL
    Version for AWS Batch with
     - 5 iteration, 270Hz sampling rate & 90Hz SH48 update rate

--- glao/src/bin/tomography_pol.rs
+++ glao/src/bin/tomography_pol.rs
@@ -5,9 +5,19 @@ use ceo::{
 };
 use cfd;
 use log::LevelFilter;
+use serde::{Deserialize, Serialize};
 use serde_pickle as pickle;
 use simple_logger::SimpleLogger;
 use std::{env, fs::File, time::Instant};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Results {
+    time: Vec<f64>,
+    wfe: Vec<(Vec<f32>, Vec<f32>, Vec<f32>)>,
+    atm_fwhm_x: f64,
+    fwhm: Vec<f64>,
+    pssn: Vec<f32>,
+}
 
 enum Loop {
     Closed,
@@ -51,8 +61,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("DURATION parsing failed!");
 
     //let _science_field = env::var("SCIENCE").expect("SCIENCE:[ONAXIS,DELAUNAY21] env var mission");
-    let rate = 40;
-    let _upload_results = true;
+    let rate = 54;
+    let upload_results = true;
 
     let cfd_case = &cfd::get_cases()?[job_idx];
     println!("CFD CASE: {} with {} duration", cfd_case, duration);
@@ -81,31 +91,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .set_guide_star(&gs_blueprint)
         .set_mmse_star(&src_blueprint)
         .set_n_side_lenslet(n_actuator - 1)
+        .set_fov_diameter(10f64.from_arcmin())
         .build();
+    let kln = lmmse.calibrate_karhunen_loeve(n_kl, None, None);
 
     let mut gmt = gmt_blueprint.build();
-    let n_px = 769;
-    let mut atm = atm_blueprint
-        //.remove_turbulence_layer(0)
-        .set_single_turbulence_layer(0f32, Some(7f32), Some(0f32))
-        .set_ray_tracing(
-            25.5,
-            n_px as i32,
-            20f32.from_arcmin(),
-            20f32,
-            Some("single_layer_atmosphere.bin".to_owned()),
-            Some(20),
-        )
-        .build();
     let mut gs = gs_blueprint.build();
     let mut wfs = wfs_blueprint.build::<Geometric>();
-    let kln = lmmse.calibrate_karhunen_loeve(n_kl, None, None);
 
     let wfs_intensity_threshold = 0.5;
     gs.through(&mut gmt).xpupil();
     wfs.calibrate(&mut gs, wfs_intensity_threshold);
 
-    let mut src = ceo!(SOURCE, set_pupil_sampling = [n_px]);
+    let n_px = 769;
+    let mut src = ceo!(FIELDDELAUNAY21, set_pupil_sampling = [n_px]);
     src.through(&mut gmt).xpupil();
 
     let mut pssn = CEO::<PSSN>::new().build::<PE>(&mut src);
@@ -121,12 +120,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         atm_fwhm_n[0].to_arcsec()
     );
 
-    //let mut data = vec![];
+    let mut atm = atm_blueprint
+        .remove_turbulence_layer(0)
+        //.set_single_turbulence_layer(0f32, Some(7f32), Some(0f32))
+        .set_ray_tracing(
+            25.5,
+            n_px as i32,
+            20f32.from_arcmin(),
+            20f32,
+            Some("glao_fiducial_atmosphere.bin".to_owned()),
+            Some(20),
+        )
+        .build();
 
-    let closed_loop_estimator = Estimator::LSQ;
-
+    let closed_loop_estimator = Estimator::LMMSE;
+    lmmse.set_n_iteration(5);
+    let wfs_rate = 18;
+    let rate_ratio = rate / wfs_rate;
     let gain = 0.5;
     let mut kl_coefs = vec![0f64; n_kl * 7];
+    let mut t: Vec<f64> = vec![];
+    let mut wfe: Vec<(Vec<f32>, Vec<f32>, Vec<f32>)> = vec![];
     let now = Instant::now();
     match glao_loop {
         Loop::Open => {
@@ -179,12 +193,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         //println!("WFE RMS: {}nm", src.wfe_rms_10e(-9)[0]);
                         //println!("WFE RMS: {:?}nm", src.segment_wfe_rms_10e(-9));
 
-                        wfs.reset();
-                        gs.through(&mut gmt)
-                            .xpupil()
-                            .through(&mut ds)
-                            .through(&mut atm)
-                            .through(&mut wfs);
+                        for _ in 0..rate_ratio {
+                            wfs.reset();
+                            gs.through(&mut gmt)
+                                .xpupil()
+                                .through(&mut ds)
+                                .through(&mut atm)
+                                .through(&mut wfs);
+                            match ds.next() {
+                                Some(p) => {
+                                    if p == rate {
+                                        println!("Step #{}: reset PSSn!", p);
+                                        //pssn.reset();
+                                    }
+                                }
+                                None => break,
+                            };
+                        }
                         wfs.process();
 
                         let mut kl_coefs_residuals: Vec<f32> =
@@ -197,16 +222,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                                 *a.0 = -gain * a.1 as f64;
                             });
                         gmt.set_m2_modes(&mut kl_coefs);
-
-                        match ds.next() {
-                            Some(p) => {
-                                if p == rate {
-                                    println!("Step #{}: reset PSSn!", p);
-                                    //pssn.reset();
-                                }
-                            }
-                            None => break,
-                        };
                     }
                 }
                 Estimator::LMMSE => {
@@ -221,22 +236,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     );
                     println!("M2 calibration in {}s", now.elapsed().as_secs());
                     loop {
+                        t.push(ds.current_time);
                         atm.secs = ds.current_time;
 
-                        src.through(&mut gmt)
+                        let wfe_rms = src
+                            .through(&mut gmt)
                             .xpupil()
                             .through(&mut ds)
                             .through(&mut atm)
-                            .through(&mut pssn);
-                        //println!("WFE RMS: {}nm", src.wfe_rms_10e(-9)[0]);
-                        //println!("WFE RMS: {:?}nm", src.segment_wfe_rms_10e(-9));
+                            .through(&mut pssn)
+                            .wfe_rms_10e(-9);
+                        wfe.push((
+                            wfe_rms,
+                            src.segment_wfe_rms_10e(-9),
+                            src.segment_piston_10e(-9),
+                        ));
 
-                        wfs.reset();
-                        gs.through(&mut gmt)
-                            .xpupil()
-                            .through(&mut ds)
-                            .through(&mut atm)
-                            .through(&mut wfs);
+                        let mut frame = rate_ratio;
+                        let state = loop {
+                            wfs.reset();
+                            gs.through(&mut gmt)
+                                .xpupil()
+                                .through(&mut ds)
+                                .through(&mut atm)
+                                .through(&mut wfs);
+                            match ds.next() {
+                                None => break None,
+                                Some(p) => {
+                                    frame -= 1;
+                                    if frame == 0 {
+                                        break Some(p);
+                                    }
+                                }
+                            }
+                        };
+                        match state {
+                            Some(p) => {
+                                if p == rate {
+                                    println!("Step #{}: reset PSSn!", p);
+                                    //pssn.reset();
+                                }
+                            }
+                            None => break,
+                        };
                         wfs.process();
 
                         wfs.centroids -= &m2_2_wfs.poke
@@ -253,65 +295,70 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                                 *a.0 = (1f64 - gain) * *a.0 - gain * a.1;
                             });
                         gmt.set_m2_modes(&mut kl_coefs);
-
-                        match ds.next() {
-                            Some(p) => {
-                                if p == rate {
-                                    println!("Step #{}: reset PSSn!", p);
-                                    //pssn.reset();
-                                }
-                            }
-                            None => break,
-                        };
                     }
                 }
             }
         }
     }
     println!(
-        "Closed-loop [{}steps/{:.3}s] in {}s",
+        " [{}steps/{:.3}s] in {}s",
         ds.n_step,
         ds.current_time,
         now.elapsed().as_secs()
     );
-    println!("WFE RMS: {}nm", src.wfe_rms_10e(-9)[0]);
-    println!("WFE RMS: {:?}nm", src.segment_wfe_rms_10e(-9));
-    pssn.peek();
-    println!("PSSn: {}", pssn);
-    let tomo_fwhm = fwhm.from_complex_otf(&pssn.telescope_error_otf());
-    println!(
-        "FWHM: {:?}",
-        tomo_fwhm.iter().map(|x| x.to_arcsec()).collect::<Vec<_>>()
-    );
-    let mut file = File::create("phase.pkl").unwrap();
-    pickle::to_writer(&mut file, &src.phase(), true).unwrap();
-    println!("OPEN LOOP STATE:");
-    gmt.reset();
-    src.through(&mut gmt)
-        .xpupil()
-        .through(&mut ds)
-        .through(&mut atm);
-    println!("WFE RMS: {}nm", src.wfe_rms_10e(-9)[0]);
-    println!("WFE RMS: {:?}nm", src.segment_wfe_rms_10e(-9));
-
-    //    let mut file = File::create("otf.pkl").unwrap();
-    //    pickle::to_writer(&mut file, &pssn.telescope_error_otf(), true).unwrap();
-
-    /*
-    let phase = Vec::<f32>::from(lmmse_phase);
-    let mut file = File::create("tomography.pkl").unwrap();
-    pickle::to_writer(&mut file, &(src_phase, phase), true).unwrap();
-
-    let src_phase: Vec<f32> = src.phase_as_ptr().into();
-    let mut file = File::create("SRC_phase.pkl").unwrap();
-    pickle::to_writer(&mut file, &src_phase, true).unwrap();
-
-    src.through(&mut gmt).xpupil().through(&mut atm);
-    println!("KL residual WFE RMS: {}nm", src.wfe_rms_10e(-9)[0]);
-
-    let kl_phase: Vec<f32> = src.phase_as_ptr().into();
-    let mut file = File::create("KL_phase.pkl").unwrap();
-    pickle::to_writer(&mut file, &kl_phase, true).unwrap();
-    */
+    if upload_results {
+        let atm_fwhm_x =
+            ceo::Fwhm::atmosphere(pssn.wavelength as f64, pssn.r0() as f64, pssn.oscale as f64)
+                .to_arcsec();
+        let glao_fwhm = fwhm.from_complex_otf(&pssn.telescope_error_otf());
+        let results = Results {
+            time: t,
+            wfe,
+            atm_fwhm_x,
+            fwhm: glao_fwhm
+                .into_iter()
+                .map(|x| x.to_arcsec())
+                .collect::<Vec<f64>>(),
+            pssn: pssn.peek().estimates.clone(),
+        };
+        let key = format!(
+            "{}/{}/glao_{}_{}.pkl",
+            "Baseline2020",
+            cfd_case,
+            match glao_loop {
+                Loop::Open => "open",
+                Loop::Closed => "closed",
+            },
+            match closed_loop_estimator {
+                Estimator::LMMSE => "lmmse",
+                Estimator::LSQ => "lsq",
+            }
+        );
+        println!("Uploading results to: s3://gmto.modeling/{}", key);
+        cirrus::dump("us-west-2", "gmto.modeling", &key, &results).await?;
+    } else {
+        println!("WFE RMS: {}nm", src.wfe_rms_10e(-9)[0]);
+        println!("WFE RMS: {:?}nm", src.segment_wfe_rms_10e(-9));
+        pssn.peek();
+        println!("PSSn: {}", pssn);
+        let tomo_fwhm = fwhm.from_complex_otf(&pssn.telescope_error_otf());
+        println!(
+            "FWHM: {:?}",
+            tomo_fwhm
+                .iter()
+                .map(|x| x.to_arcsec())
+                .collect::<Vec<f64>>()
+        );
+        let mut file = File::create("phase.pkl").unwrap();
+        pickle::to_writer(&mut file, &src.phase(), true).unwrap();
+        println!("OPEN LOOP STATE:");
+        gmt.reset();
+        src.through(&mut gmt)
+            .xpupil()
+            .through(&mut ds)
+            .through(&mut atm);
+        println!("WFE RMS: {}nm", src.wfe_rms_10e(-9)[0]);
+        println!("WFE RMS: {:?}nm", src.segment_wfe_rms_10e(-9));
+    }
     Ok(())
 }
