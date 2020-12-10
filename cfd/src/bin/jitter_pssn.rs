21c3e57bc6aad34e42cd1f14da8fa8b09436d3c1
Rod Conan <rconan@gmto.org>
2020-12-10 05:23:28 +0000

    TOKIO MULTITASKING JITTER PSSN

--- cfd/src/bin/jitter_pssn.rs
+++ cfd/src/bin/jitter_pssn.rs
@@ -1,10 +1,10 @@
-use ceo::{ceo, pssn::TelescopeError, Builder, CEOType, PSSN, SOURCE, FIELDDELAUNAY21};
+use ceo::{ceo, pssn::TelescopeError, set_gpu, Builder, FIELDDELAUNAY21, PSSN, SOURCE};
 use cirrus;
-use indicatif::{ProgressBar, ProgressStyle};
 use serde::{Deserialize, Serialize};
 use serde_pickle as pickle;
+use serde_yaml as yaml;
+use std::collections::BTreeMap;
 use std::fs::File;
-use std::io::BufReader;
 
 #[derive(Debug, Deserialize, Default)]
 struct FemRbm {
@@ -31,48 +31,29 @@ struct Data {
     #[serde(rename = "Data")]
     data: TimeSeries,
 }
-#[derive(Debug, Serialize, Default)]
+#[derive(Debug, Serialize, Deserialize, Default)]
 struct Results {
     v_band: Band,
     h_band: Band,
 }
-#[derive(Debug, Serialize, Default)]
+#[derive(Debug, Serialize, Deserialize, Default)]
 struct Band {
     pssn: Vec<Vec<f32>>,
     psu: f32,
 }
 
-#[tokio::main]
-async fn main() {
-    let mut gmt = ceo!(GMT, set_m1_n_mode = [27]);
-    let _src_ = FIELDDELAUNAY21::new();
-    let mut src = _src_.clone().build();
-    let mut pssn = PSSN::<TelescopeError>::new().set_source(_src_).build();
-    src.through(&mut gmt).xpupil().through(&mut pssn);
-    println!(
-        "WFE RMS: {:?}nm ; PSSn: {}",
-        src.wfe_rms_10e(-9),
-        pssn.peek()
-    );
-
-    let use_s3 = true;
-    let data: Data = if use_s3 {
-        let cfd_case = "b2019_0z_0az_cd_12ms";
-        let key = format!(
-            "{}/{}/MT_FSM_IO_FSM_MountCtrl_TT7.rs.pkl",
-            "Baseline2020", cfd_case
-        );
-        let r = cirrus::reader("us-east-2", "gmto.starccm", &key).await.unwrap();
-        pickle::from_reader(r).expect("Failed")
-    } else {
-        let filename = "/home/ubuntu/DATA/MT_FSM_IO_FSM_MountCtrl_TT7.rs.pkl";
-        let f = File::open(filename).unwrap();
-        let r = BufReader::with_capacity(1000_0000, f);
-        pickle::from_reader(r).expect("Failed")
-    };
+fn rbm_to_pssn(data: Data) -> Band {
     let mut fem: FemRbm = data.data.fem;
     let n_sample = fem.m1_rbm.len();
-    println!("# sample: {}", n_sample);
+    //println!("# sample: {}", n_sample);
+
+    let mut gmt = ceo!(GMT, set_m1_n_mode = [27]);
+    let _src_ = FIELDDELAUNAY21::new().set_band("H");
+    let mut src = _src_.clone().build();
+    let mut pssn = PSSN::<TelescopeError>::new().set_source(_src_).build();
+    //println!("r0: {}m", pssn.r0());
+    src.through(&mut gmt).xpupil().through(&mut pssn);
+    pssn.reset();
 
     let sampling_time = 0.5e-3_f64;
     let peek_time = 1_f64;
@@ -80,42 +61,79 @@ async fn main() {
     let k0: usize = 10_000;
     let step: usize = 50;
 
-    let n_step = (n_sample - k0) / step;
-    println!("# step: {}", n_step);
+    //let n_step = (n_sample - k0) / step;
+    //println!("# step: {}", n_step);
     let mut pssn_sec: Vec<Vec<f32>> = vec![];
     let rbm = fem
         .m1_rbm
         .drain(k0..)
         .step_by(step)
         .zip(fem.m2_rbm.drain(k0..).step_by(step));
-    let bar = ProgressBar::new(n_step as u64);
-    bar.set_style(ProgressStyle::default_bar().template("{eta} {bar:40.cyan/blue} {msg}"));
     for (k, (m1_rbm, m2_rbm)) in rbm.enumerate() {
-        bar.inc(1);
         gmt.update42(Some(&m1_rbm), Some(&m2_rbm), None, None);
-        src.through(&mut gmt).xpupil();
-        pssn.accumulate(&mut src);
+        src.through(&mut gmt).xpupil().through(&mut pssn);
         if (k * step % peek_lag) == 0 {
             pssn.peek();
             pssn_sec.push(pssn.estimates.clone());
-            //println!("PSSn[{}]: {}", k, pssn);
-            bar.set_message(&format!("{}", pssn.estimates[0]));
         }
     }
-    bar.finish();
-    let psu = pssn.spatial_uniformity();
+    pssn.peek();
     pssn_sec.push(pssn.estimates.clone());
-    let results = Results {
-        v_band: Band {
-            pssn: pssn_sec.clone(),
-            psu: psu,
-        },
-        h_band: Band {
-            pssn: pssn_sec,
-            psu: psu,
-        },
-    };
-    let filename = "jitter.pssn.pkl";
-    let mut file = File::create(filename).unwrap();
-    pickle::to_writer(&mut file, &results, true).unwrap();
+    let psu = pssn.spatial_uniformity();
+    Band {
+        pssn: pssn_sec,
+        psu: psu,
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let n_gpu = 1 as usize;
+
+    let filename = "/home/ubuntu/CFD/CFD_CASES.yaml";
+    let r = File::open(filename).unwrap();
+    let mut cfd_cases: BTreeMap<String, Vec<String>> = yaml::from_reader(r).unwrap();
+    let mut b2020_cases = cfd_cases.remove("baseline 2020").unwrap();
+    b2020_cases.truncate(1);
+    println!("CFD CASES: {:?}", b2020_cases);
+
+    let mut handle = vec![];
+    let cfd_cases = b2020_cases.clone();
+    for (k, c) in cfd_cases.into_iter().enumerate() {
+        handle.push(tokio::spawn(async move {
+            set_gpu((k % n_gpu) as i32);
+            let data: Data = {
+                let key = format!(
+                    "{}/{}/MT_FSM_IO_FSM_MountCtrl_TT7.rs.pkl",
+                    "Baseline2020", c
+                );
+                let r = cirrus::reader("us-east-2", "gmto.starccm", &key)
+                    .await
+                    .unwrap();
+                pickle::from_reader(r).expect("Failed")
+            };
+            rbm_to_pssn(data)
+        }))
+    }
+    for (h,c) in handle.iter_mut().zip(b2020_cases.iter()) {
+        let h_band_pssn = h.await.unwrap();
+        let key = format!(
+            "{}/{}/MT_FSM_IO_FSM_MountCtrl_TT7.pssn.pkl",
+            "Baseline2020", c
+        );
+        let v_band_pssn: Band = {
+            let r = cirrus::reader("us-east-2", "gmto.starccm", &key)
+                .await
+                .unwrap();
+            pickle::from_reader(r).expect("Failed")
+        };
+        let results = Results {
+            v_band: v_band_pssn,
+            h_band: h_band_pssn,
+        };
+
+        cirrus::dump("us-east-2", "gmto.starccm", &key, &results)
+            .await
+            .unwrap();
+    }
 }
